512c1b975f6df2b0c82275292004a0e3011b5b58
Marco Rizzi <mrizzi@users.noreply.github.com>
2021-04-02 11:21:47 +0200

    Changed name from 'headers' to 'queryParameters' (#39)

--- commons-rest-sample/src/test/java/io/tackle/commons/resources/query/QueryTest.java
+++ commons-rest-sample/src/test/java/io/tackle/commons/resources/query/QueryTest.java
@@ -14,12 +14,12 @@ public class QueryTest {
     
     @Test
     public void testQuery() {
-        MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
-        headers.addAll("cani.name", "a", "b");
-        headers.addAll("cats.name", "a", "b");
-        headers.addAll("fishes.foo", "a", "b");
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.addAll("cani.name", "a", "b");
+        queryParameters.addAll("cats.name", "a", "b");
+        queryParameters.addAll("fishes.foo", "a", "b");
         UriInfo uriInfo = Mockito.mock(UriInfo.class);
-        Mockito.when(uriInfo.getQueryParameters(true)).thenReturn(headers);
+        Mockito.when(uriInfo.getQueryParameters(true)).thenReturn(queryParameters);
         Query query = QueryBuilder.withPanacheEntity(Person.class).andUriInfo(uriInfo).build();
         // "SELECT table FROM Person table JOIN table.fishes fishes JOIN table.cats cats JOIN table.dogs cani WHERE ( lower(fishes.foo) LIKE lower(:fishes_foo201) or lower(fishes.foo) LIKE lower(:fishes_foo215) ) and ( lower(cats.name) LIKE lower(:cats_name562) or lower(cats.name) LIKE lower(:cats_name71) ) and ( lower(cani.name) LIKE lower(:cani_name284) or lower(cani.name) LIKE lower(:cani_name841) )"
         assertTrue(query.getQuery().startsWith("SELECT table FROM Person table JOIN table.fishes fishes JOIN table.cats cats JOIN table.dogs cani WHERE "));
