25d2828c994591752701312b6a2498bee330a22e
mrizzi <mrizzi@redhat.com>
2022-04-28 17:50:31 +0200

    TACKLE-507 Update Tackle Repo licenses to APL 2.0

--- commons-rest-sample/src/main/resources/import.sql
+++ commons-rest-sample/src/main/resources/import.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright © 2021 Konveyor (https://konveyor.io/)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 INSERT INTO breed(id, name, origin, internationalId, deleted) VALUES (nextval('hibernate_sequence'), 'i', 'j', 123, false);
 INSERT INTO person(id, name, deleted) VALUES (nextval('hibernate_sequence'), 'c', false );
 INSERT INTO dog(id, name, color, breed_id, deleted, owner_id) VALUES (nextval('hibernate_sequence'), 'a', 'b', 1, false, 2);
