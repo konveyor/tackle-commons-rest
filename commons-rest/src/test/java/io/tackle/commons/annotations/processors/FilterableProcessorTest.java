48deb08bb41d5c7cc461e3a8beb7bde02fcd5965
Marco Rizzi <mrizzi@users.noreply.github.com>
2022-04-29 12:56:44 +0200

    TACKLE-507 Update Tackle Repo licenses to APL 2.0 (#154)
    * TACKLE-507 Update Tackle Repo licenses to APL 2.0
    
    * TACKLE-507 Enhanced expected result in FilterableProcessorTest after having added the header
    
    * TACKLE-507 Update Tackle Repo licenses to APL 2.0

--- commons-rest/src/test/java/io/tackle/commons/annotations/processors/FilterableProcessorTest.java
+++ commons-rest/src/test/java/io/tackle/commons/annotations/processors/FilterableProcessorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.tackle.commons.annotations.processors;
 
 import com.google.testing.compile.Compilation;
@@ -22,7 +37,7 @@ public class FilterableProcessorTest {
         assertThat(compilation)
                 .hadErrorContaining("@Filterable must have a 'filterName' value when used with a 'OneToMany' annotated field. The format must follow the 'dot' notation (i.e. 'entities.field')")
                 .inFile(sampleEntity)
-                .onLine(9)
+                .onLine(24)
                 .atColumn(25);
     }
 }
